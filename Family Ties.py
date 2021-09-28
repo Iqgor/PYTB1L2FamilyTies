1c6370cc2c24b7c44ed670f9fe89129d4e61d959
Igor <33993@ma-web.nl>
2021-09-28 09:43:39 +0200

    Eerste commit

---
+++ Family Ties.py
@@ -0,0 +1,16 @@
+def start():
+    a = "Igor"
+    b = 17
+    c = "Lelystad en Almere."
+    d = 45.5
+    e = 1
+    f = 1
+    g = "wielrenen"
+    h = "Hockey."
+    print("Ik ben",a ,"ben",b,"jaar oud en ik woon in",c,"Dat is zo'n",d,"kilometer van school!")
+    print("Ik woon daar met",e,"zusje en",f,"kat.")
+    print("Ik doe graag aan sport ik doe dan graag",g,"en vroeger",h)
+
+
+
+start()
